ff4c8469ab9bd80e9e62681d8a1dfaee498c83a3
Yiting Li <lyiting@astro-lyiting-mba.localdomain>
2024-11-04 10:39:14 -0500

    update

--- yield.py
+++ yield.py
@@ -470,8 +470,8 @@ if st.button('Run'):
                 bd_detected_fraction[ii]+=bd_detection[jj]
             bd_detection[jj]=0
             
-#Fout.close()
-#Fout1.close()
+Fout.close()
+Fout1.close()
 ##
 
 #    #######################
