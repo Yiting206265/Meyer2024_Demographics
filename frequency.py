e94e9bafb0602d56e7d8cbe078fccf8d274aa175
Yiting Li <lyiting@astro-lyiting-mba.localdomain>
2024-11-06 14:56:31 -0500

    update

--- frequency.py
+++ frequency.py
@@ -6,27 +6,26 @@ import operator
 import matplotlib.pyplot as plt
 
 # Constants and model parameters for Brown Dwarfs
-A_bd_ln = -4.08
+A_bd_ln = -3.78
 mean_bd = 1.43   # log10
 sigma_bd = 1.21  # log10
 A_bd = np.exp(A_bd_ln)
+alpha_bd = -0.36
 
 # Constants and model parameters for Giant Planets
-A_pl_ln = -5.28
-mu_natural = 1.31     #ln
-sigma_pl_ln = np.exp(0.52)  #ln
+alpha_gp = 1.43
+A_pl_ln = -5.52
+mu_natural = 1.32     #ln
+sigma_pl_ln = 0.53    #ln
+A_pl = np.exp(A_pl_ln)
 
 # Conversion constant from natural log to log_10
 constant = 2.302585092994046
-
-# Convert parameters for Giant Planets to log_10 scale
-A_pl = np.exp(A_pl_ln)
 mu_pl = mu_natural / constant  # log10
 sigma_pl = sigma_pl_ln / constant # log10
 
 # Display model parameters in Streamlit
 st.header("Two Component Model for Exoplanet and Brown Dwarf Companions (Meyer et al. 2024)")
-
 st_type = st.radio("Choose the Stellar Spectral Type:", ("M Dwarfs", "FGK", "A Stars"))
 
 # User input for mass parameters
@@ -37,22 +36,19 @@ Jup_max = st.slider("Companion Maximum Mass ($\mathrm{M_{Jup}}$)", min_value=0.0
 # Mass ratio calculations
 q_Jupiter = 0.001 / host_mass
 d_q = np.logspace(-5, 3, 500)  # Mass ratios from 0.0001 to 1 on a logarithmic scale
-a1 = d_q
 
 # Brown Dwarf model distribution (mass ratio)
-alpha_bd = -0.3
 a2_bd = d_q ** alpha_bd
 
 # Giant Planet model distribution (mass ratio)
-alpha_gp = 1.39
 a2_gp = d_q ** -alpha_gp
 
 # Create plots for both distributions side by side
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17, 6))
 
 # Plotting normalized mass ratio distributions
-ax1.plot(a1, a2_bd, color='r', label='Brown Dwarf Model')
-ax1.plot(a1, a2_gp, color='blue', label='Giant Planet Model')
+ax1.plot(d_q, a2_bd, color='r', label='Brown Dwarf Model')
+ax1.plot(d_q, a2_gp, color='blue', label='Giant Planet Model')
 
 ax1.set_xscale('log')
 ax1.set_yscale('log')
@@ -74,7 +70,7 @@ ax1.set_title("Mass Ratio Distributions", fontsize=18)
 
 # User input for orbital separations using sliders
 a_min = st.slider("Orbital Minimum Separation (AU)", min_value=0.01, max_value=100.0, value=1.0)
-a_max = st.slider("Orbital Maximum Separation (AU)", min_value=0.01, max_value=1000.0, value=10.0)
+a_max = st.slider("Orbital Maximum Separation (AU)", min_value=0.01, max_value=2000.0, value=10.0)
 
 if st_type == "M Dwarfs":
     s_m =  np.log(10**1.21)    # Winters
@@ -131,16 +127,15 @@ f_pl = A_pl * np.trapz([orbital_dist_pl(a)/(np.sqrt(2*np.pi)*sigma_pl_ln*a) for 
 st.write(f"Frequency of Planets:", f_pl)
 st.write(f"Frequency of Brown Dwarfs:", f_bd)
 
-
 # Sub-Jupiter Model
 st.subheader("Sub-Jupiters (< 1 MJ)")
-st.write(f"For planets less than 1 Jupiter mass, we made a customized sub-Jupiter model where the orbital distribution within 10 AU is the same as the giant planet model, but log-flat beyond 10 AU.")
+st.write(f"For planets less than 1 Jupiter mass, we made a customized sub-Jupiter model where the orbital distribution within 7 AU is twice the giant planet model (Fulton 2021), but log-flat with a constant density beyond 7 AU.")
 
 # Create plots for both distributions side by side
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17, 6))
 
-# Plotting normalized mass ratio distributions
-ax1.plot(a1, a2_gp, color='blue', label='Giant Planet Model')
+# Plotting mass ratio distributions
+ax1.plot(d_q, a2_gp, color='blue', label='Giant Planet Model')
 
 ax1.set_xscale('log')
 ax1.set_yscale('log')
@@ -157,18 +152,16 @@ ax1.axvline(x=mass_ratio_max, color='purple', linestyle='--', label=f'Max Mass R
 ax1.set_xlabel('Mass Ratio q', fontsize=20, labelpad=10.4)
 ax1.set_ylabel('Probability Density', fontsize=20, labelpad=10.4)
 ax1.tick_params(axis='both', which='major', labelsize=15)
-#ax1.set_xlim(np.log10(0.0001), np.log10(1))  # Adjust x-limits
-#ax1.set_ylim(0, 1.1)
 ax1.legend(loc='upper right', fontsize=12)
 ax1.set_title("Mass Ratio Distributions", fontsize=18)
 
 
-# Define orbital separation distributions for Brown Dwarfs and Giant Planets
+# Define orbital separation distributions for sub-Jupiters
 def orbital_dist_subJupiter(a):
-    if a <= 10:
-        return (np.exp(-(np.log(a) - mu_natural) ** 2 / (2 * sigma_pl_ln ** 2)))#/(np.sqrt(2*np.pi)*sigma_pl_ln*a)
+    if a <= 7:
+        return 2*(np.exp(-(np.log(a) - mu_natural) ** 2 / (2 * sigma_pl_ln ** 2)))#/(np.sqrt(2*np.pi)*sigma_pl_ln*a)
     else:
-        return 0.84
+        return 0.8430271150978883
 
 # Create a log-scaled range for plotting
 a_values = np.logspace(-6, 10, 500, base=2.71828)  # Values from 0.01 to 1000, log-scaled
@@ -186,7 +179,7 @@ ax2.set_ylabel('Probability Density', fontsize=20, labelpad=10.4)
 ax2.tick_params(axis='both', which='major', labelsize=15)
 ax2.legend(loc='upper right', fontsize=12)
 ax2.set_title("Semi-Major Axis Distributions", fontsize=18)
-ax2.set_ylim(0,1.5)
+ax2.set_ylim(0,3)
 # Display the plots
 st.pyplot(fig)
 
